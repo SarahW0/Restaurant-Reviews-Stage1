1ffc0c52ce8a09a526fed4f0fc73f9553505b382
Sarah Wang <wgjhxx@gmail.com>
2019-06-12 21:11:36 +1000

    update dbhelper.js
    add cross origin support

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,188 +1,84 @@
-/**
- * Common database helper functions.
- */
-class DBHelper {
-  /**
-   * Database URL.
-   * Change this to restaurants.json file location on your server.
-   */
-  static get DATABASE_URL() {
-    const port = 5500; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
-  }
+const cacheName = "restaurant-review-stage1-v1-4";
 
-  /**
-   * Fetch all restaurants.
-   */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else {
-        // Oops!. Got an error from server.
-        const error = `Request failed. Returned status of ${xhr.status}`;
-        callback(error, null);
+//self is the worker here and is the window somewhere else
+self.addEventListener("install", event => {
+  const resourceToCache = [
+    "/",
+    "index.html",
+    "restaurant.html",
+    "css/styles.css",
+    "data/restaurants.json",
+    "js/dbhelper.js",
+    "js/main.js",
+    "js/restaurant_info.js"
+  ];
+
+  console.log("install event");
+  //wait the last promise to complete before returning control to the event handler
+  event.waitUntil(
+    //add the resource to the cache
+    caches
+      .open(cacheName)
+      .then(cache => {
+        //load the resources from network and save them in the cache
+        return cache.addAll(resourceToCache);
+      })
+      .catch(err => console.log(err))
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(cache) {
+            // Return true if you want to remove this cache,
+            // but remember that caches are shared across
+            // the whole origin
+
+            return cache.indexOf("restaurant-review") >= 0;
+          })
+          .map(function(cache) {
+            console.log("delete:", cache);
+            return caches.delete(cache);
+          })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    //find the fetching resource from the cache
+    caches.match(event.request.url).then(cachedResponse => {
+      const fetchURL = new URL(event.request.url);
+      if (fetchURL.hostname !== "localhost") {
+        event.request.mode = "no-cors";
       }
-    };
-    xhr.send();
-  }
+      return (
+        cachedResponse ||
+        fetch(event.request)
+          .then(function(response) {
+            if (response.status === 404) {
+              return new Response("Resource was not found!");
+            }
+            return caches.open(cacheName).then(cache => {
+              //load the resources from network and save them in the cache
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          })
+          .catch(function(err) {
+            return new Response(`Something is wrong: ${err}`);
+          })
+      );
+    })
+  );
+});
 
-  /**
-   * Fetch a restaurant by its ID.
-   */
-  static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) {
-          // Got the restaurant
-          callback(null, restaurant);
-        } else {
-          // Restaurant does not exist in the database
-          callback("Restaurant does not exist", null);
-        }
-      }
-    });
+self.addEventListener("message", function(event) {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
   }
-
-  /**
-   * Fetch restaurants by a cuisine type with proper error handling.
-   */
-  static fetchRestaurantByCuisine(cuisine, callback) {
-    // Fetch all restaurants  with proper error handling
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        // Filter restaurants to have only given cuisine type
-        const results = restaurants.filter(r => r.cuisine_type == cuisine);
-        callback(null, results);
-      }
-    });
-  }
-
-  /**
-   * Fetch restaurants by a neighborhood with proper error handling.
-   */
-  static fetchRestaurantByNeighborhood(neighborhood, callback) {
-    // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        // Filter restaurants to have only given neighborhood
-        const results = restaurants.filter(r => r.neighborhood == neighborhood);
-        callback(null, results);
-      }
-    });
-  }
-
-  /**
-   * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
-   */
-  static fetchRestaurantByCuisineAndNeighborhood(
-    cuisine,
-    neighborhood,
-    callback
-  ) {
-    // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        let results = restaurants;
-        if (cuisine != "all") {
-          // filter by cuisine
-          results = results.filter(r => r.cuisine_type == cuisine);
-        }
-        if (neighborhood != "all") {
-          // filter by neighborhood
-          results = results.filter(r => r.neighborhood == neighborhood);
-        }
-        callback(null, results);
-      }
-    });
-  }
-
-  /**
-   * Fetch all neighborhoods with proper error handling.
-   */
-  static fetchNeighborhoods(callback) {
-    // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        // Get all neighborhoods from all restaurants
-        const neighborhoods = restaurants.map(
-          (v, i) => restaurants[i].neighborhood
-        );
-        // Remove duplicates from neighborhoods
-        const uniqueNeighborhoods = neighborhoods.filter(
-          (v, i) => neighborhoods.indexOf(v) == i
-        );
-        callback(null, uniqueNeighborhoods);
-      }
-    });
-  }
-
-  /**
-   * Fetch all cuisines with proper error handling.
-   */
-  static fetchCuisines(callback) {
-    // Fetch all restaurants
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        // Get all cuisines from all restaurants
-        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
-        // Remove duplicates from cuisines
-        const uniqueCuisines = cuisines.filter(
-          (v, i) => cuisines.indexOf(v) == i
-        );
-        callback(null, uniqueCuisines);
-      }
-    });
-  }
-
-  /**
-   * Restaurant page URL.
-   */
-  static urlForRestaurant(restaurant) {
-    return `./restaurant.html?id=${restaurant.id}`;
-  }
-
-  /**
-   * Restaurant image URL.
-   */
-  static imageUrlForRestaurant(restaurant) {
-    return `/img/${restaurant.photograph}`;
-  }
-
-  /**
-   * Map marker for a restaurant.
-   */
-  static mapMarkerForRestaurant(restaurant, map) {
-    // https://leafletjs.com/reference-1.3.0.html#marker
-    const marker = new L.marker(
-      [restaurant.latlng.lat, restaurant.latlng.lng],
-      {
-        title: restaurant.name,
-        alt: restaurant.name,
-        url: DBHelper.urlForRestaurant(restaurant),
-        keyboard: false
-      }
-    );
-    marker.addTo(newMap);
-    return marker;
-  } 
-}
+});
